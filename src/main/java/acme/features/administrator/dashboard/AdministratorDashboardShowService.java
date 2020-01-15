d175e077f320ebd2f39bfb48c0c98bca979f4f4a
Carlos Santos Tirado <carsantir@gmail.com>
2020-01-15 18:58:33 +0100

    Task arreglo:finished

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
@@ -55,31 +55,66 @@ public class AdministratorDashboardShowService implements AbstractShowService<Ad
 		result.setMaxRewardRequest(this.repository.getMaxRewardRequest());
 
 		Money a = new Money();
-		a.setAmount(this.repository.getAverageRewardRequestAmount());
-		a.setCurrency(this.repository.getAverageRewardRequestCurrency());
+		Double amountA = this.repository.getAverageRewardRequestAmount();
+		String currencyA = this.repository.getAverageRewardRequestCurrency();
+		if (amountA != null && currencyA != null) {
+			a.setAmount(this.repository.getAverageRewardRequestAmount());
+			a.setCurrency(this.repository.getAverageRewardRequestCurrency());
+		} else {
+			a.setAmount(0.);
+			a.setCurrency("€");
+		}
 		result.setAverageRewardRequest(a);
 
 		Money c = new Money();
-		c.setAmount(this.repository.getSdRewardRequestAmount());
-		c.setCurrency(this.repository.getAverageRewardRequestCurrency());
+		Double amountC = this.repository.getSdRewardRequestAmount();
+		String currencyC = this.repository.getAverageRewardRequestCurrency();
+		if (amountC != null && currencyC != null) {
+			c.setAmount(this.repository.getSdRewardRequestAmount());
+			c.setCurrency(this.repository.getAverageRewardRequestCurrency());
+		} else {
+			c.setAmount(0.);
+			c.setCurrency("€");
+		}
 		result.setSdRewardRequest(c);
 
 		result.setMinRewardOffer(this.repository.getMinRewardOffer());
 		result.setMaxRewardOffer(this.repository.getMaxRewardOffer());
 
 		Money b = new Money();
-		b.setAmount(this.repository.getAverageRewardOfferAmount());
-		b.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		Double amountB = this.repository.getAverageRewardOfferAmount();
+		String currencyB = this.repository.getAverageRewardOfferCurrency();
+		if (amountB != null && currencyB != null) {
+			b.setAmount(this.repository.getAverageRewardOfferAmount());
+			b.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		} else {
+			b.setAmount(0.);
+			b.setCurrency("€");
+		}
 		result.setAverageRewardOffer(b);
 
 		Money d = new Money();
-		d.setAmount(this.repository.getMinSdRewardRequestAmount());
-		d.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		Double amountD = this.repository.getMinSdRewardRequestAmount();
+		String currencyD = this.repository.getAverageRewardOfferCurrency();
+		if (amountD != null && currencyD != null) {
+			d.setAmount(this.repository.getMinSdRewardRequestAmount());
+			d.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		} else {
+			d.setAmount(0.);
+			d.setCurrency("€");
+		}
 		result.setSdMinRewardOffer(d);
 
 		Money e = new Money();
-		e.setAmount(this.repository.getMaxSdRewardRequestAmount());
-		e.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		Double amountE = this.repository.getMaxSdRewardRequestAmount();
+		String currencyE = this.repository.getAverageRewardOfferCurrency();
+		if (amountE != null && currencyE != null) {
+			e.setAmount(this.repository.getMaxSdRewardRequestAmount());
+			e.setCurrency(this.repository.getAverageRewardOfferCurrency());
+		} else {
+			e.setAmount(0.);
+			e.setCurrency("€");
+		}
 		result.setSdMaxRewardOffer(e);
 
 		result.setCompaniesPerSector(this.repository.getCompaniesPerSector());
